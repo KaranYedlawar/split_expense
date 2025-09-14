6c3bc98c972ccd3b1c47f7cc38bdf32b841ab540
Karan Yedlawar <karanyedlawar786@gmail.com>
2025-09-15 04:05:11 +0530

    seed file updated to have default 3 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,16 @@
 
 
 Fabricate.times(10, :user)
+
+[
+  { email: "john@example.com", name: "John" },
+  { email: "anbu@example.com", name: "Anbu" },
+  { email: "barath@example.com", name: "Barath" }
+].each do |user_attrs|
+  User.find_or_create_by!(email: user_attrs[:email]) do |user|
+    user.name = user_attrs[:name]
+    user.password = "password"
+    user.password_confirmation = "password"
+  end
+end
+
