deff45bba8baa6fd631bdf5c7da4a55e77d7990a
Karan Yedlawar <karanyedlawar786@gmail.com>
2025-09-15 08:53:10 +0530

    render setup

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-#exit on error
+
 set -o errexit
 
 bundle install
-yarn install --check-files
-bundle exec rails assets:precompile
-bundle exec rails db:migrate
+bin/rails assets:precompile
+bin/rails assets:clean
+
+bin/rails db:migrate
