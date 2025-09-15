92f20f04b41f4b332b634c3ada10bc4b9ff894cd
Karan Yedlawar <karanyedlawar786@gmail.com>
2025-09-15 15:26:05 +0530

    version compatibility fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,18 @@
 set -o errexit
 
 bundle install
+
+# Install JS dependencies so webpacker:compile can run
+yarn install --check-files
+
+# Compile webpacker packs into public/packs
 bundle exec rails webpacker:compile
+
+# Precompile Sprockets assets
 bundle exec rails assets:precompile
+
+# Clean old assets
 bundle exec rails assets:clean
+
+# Run migrations
 bundle exec rails db:migrate
